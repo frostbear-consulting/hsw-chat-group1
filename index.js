9bf5ad0e9b955a292bba7fda10131757463cf5da
David Dernovoj <david@dernovoj.com>
2023-05-03 12:50:20 +0200

    Add example for timestamp filtering

--- index.js
+++ index.js
@@ -6,15 +6,21 @@ app.use(express.json());
 app.use(express.static('static'));
 
 const messages = [
-    { sender: 1, message: 'Hallo!', time: '11:18' },
-    { sender: 2, message: 'Moin!', time: '11:19' },
-    { sender: 1, message: 'Toller Chat hier', time: '11:20' },
-    { sender: 2, message: 'Ja wirklich!', time: '11:22' },
-    { sender: 2, message: 'Pls respond', time: '11:28' },
+    { sender: 1, message: 'Hallo!', time: '11:18', ts: 1683110779177 },
+    { sender: 2, message: 'Moin!', time: '11:19', ts: 1683110779178 },
+    { sender: 1, message: 'Toller Chat hier', time: '11:20', ts: 1683110779179 },
+    { sender: 2, message: 'Ja wirklich!', time: '11:22', ts: 1683110779180 },
+    { sender: 2, message: 'Pls respond', time: '11:28', ts: 1683110779181 },
 ];
 
 app.get('/api/messages', async function (req, res) {
-    res.json(messages);
+    let output = messages;
+
+    if (req.query.since) {
+        output = output.filter(m => m.ts >= parseInt(req.query.since));
+    }
+
+    res.json(output);
 });
 
 app.post('/api/messages', async function (req, res) {
